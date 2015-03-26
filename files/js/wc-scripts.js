4d6e0262a4db93fde0ab35ac20030824936e2cad
AdvancedCoding <AdvancedCoding@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 15:16:47 +0000

    * Added : Integration with Ultimate Member - author profile picture * Added : Integration with Ultimate Member - author name is linked to profile page * Fixed Bug: CSS overrides some default styles on /wp-admin/
    git-svn-id: https://plugins.svn.wordpress.org/wpdiscuz/tags/2.1.6@1121447 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ files/js/wc-scripts.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function ($) {
+    var pickerImg1 = $('.wc_colorpicker_img1');
+    var modalBox1 = $('div#wc_box1');
+    var position1 = pickerImg1.position();
+    /*modalBox1.css('margin-left', position1.left + 200);*/
+
+    $('#wc_colorpickerHolder1').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_comment_bg_color').val('#' + hex);
+        }
+    });
+
+    $('#wc_colorpickerHolder2').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_reply_bg_color').val('#' + hex);
+        }
+    });
+
+    $('#wc_colorpickerHolder3').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_comment_text_color').val('#' + hex);
+        }
+    });
+
+    $('#wc_colorpickerHolder4').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_author_title_color').val('#' + hex);
+        }
+    });
+
+    $('#wc_colorpickerHolder5').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_vote_reply_color').val('#' + hex);
+        }
+    });
+
+    $('#wc_colorpickerHolder6').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_form_bg_color').val('#' + hex);
+        }
+    });
+
+    $('#wc_colorpickerHolder7').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_new_loaded_comment_bg_color').val('rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')');
+        }
+    });
+    
+    $('#wc_colorpickerHolder8').ColorPicker({
+        flat: true,
+        onChange: function (hsb, hex, rgb) {
+            $('#wc_input_border_color').val('#' + hex);
+        }
+    });
+});
